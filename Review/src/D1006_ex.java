7ea5bf44b90450b51e021d2926f0d92ca3da5316
hey-me <ebb@kakao.com>
2022-05-31 18:07:20 +0900

    2022/05/31

--- Review/src/D1006_ex.java
+++ Review/src/D1006_ex.java
@@ -12,7 +12,7 @@ public class D1006_ex {
 			System.arraycopy(rearr, 0, arr, 0, rearr.length);
 			arr[i+2] = arr[i] + arr[i+1];
 		}
-
+//
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
